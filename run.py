ad3d5a885df76c9309a1d008eca44c61c606de05
Kevin Nadar <kevinxaviernadar@gmail.com>
2021-12-21 13:03:39 +0530

    Optimised .gitignore and minor refactors

--- run.py
+++ run.py
@@ -7,23 +7,22 @@ Created on Thu Jul  1 11:34:09 2021
 """
 
 import gspread
-from datetime import date, datetime
+import datetime
 from Calendar import FetchEvent
 from Sheets import AttendanceRegister
 
-now = datetime.now().ctime()
+date = datetime.date.today()
+time = datetime.datetime.now().ctime()
 
-dt = date.today()\
+file = open(f'/home/kevin/.config/spyder-py3/Projects/Meet-Attendance/Logs/{date}', 'w')
 
-f = open(f'/home/kevin/.config/spyder-py3/Projects/Meet-Attendance/Logs/{dt}', 'w')
-
-f.write(now)
+file.write(time)
 
 # try:
 
 client = gspread.oauth()
 
-instance = FetchEvent(dt)
+instance = FetchEvent(date)
 
 mark = instance.events_to_attend(
     instance.latest_events, 'This Google Calendar event is going to be \
@@ -32,18 +31,18 @@ accounted in the Meet Attendance.'
 
 spreadsheet = client.open('Meet Attendance 07/02/2021')
 
-attendanceRegister = AttendanceRegister(client, spreadsheet, f)
+attendanceRegister = AttendanceRegister(client, spreadsheet, file)
 
-ev_to_ws = attendanceRegister.event_to_worksheet(mark, dt)
+ev_to_ws = attendanceRegister.event_to_worksheet(mark, date)
 
 attendanceRegister.register_attendance(ev_to_ws)
 
 attendanceRegister.delete_sheet(spreadsheet, spreadsheet.worksheets()[-1])
 
 print('Done!')
-f.write('\nDone!\n')
+file.write('\nDone!\n')
 
 # except Exception as e:
 #     print(e)
 
-f.close()
+file.close()
